2b820d1848e94b1fbfe64ea37e2bc06ee4eceadf
Ross Campbell <ross.campbell@mosaicx.com>
2025-03-24 06:53:59 -0500

    add/remove quizzes

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ import agileQuiz from "./data/agile-quiz";
 import cloudComputingQuiz from "./data/cloud-computing-quiz";
 import cssQuiz from "./data/css-quiz";
 import devopsQuiz from "./data/devops-quiz";
-import freecodecampQuiz from "./data/freecodecamp-quiz";
 import generalCSQuiz from "./data/general-cs-quiz";
 import gitQuiz from "./data/git-quiz";
 import htmlQuiz from "./data/html-quiz";
@@ -12,34 +11,42 @@ import javascriptQuiz from "./data/javascript-quiz";
 import linuxQuiz from "./data/linux-quiz";
 import pythonQuiz from "./data/python-quiz";
 import qualityAssuranceQuiz from "./data/quality-assurance-quiz";
+import regexQuiz from "./data/regex-quiz";
 import securityQuiz from "./data/security-quiz";
 import sqlQuiz from "./data/sql-quiz";
-import regexQuiz from "./data/regex-quiz";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const mapQuizCategory = (quiz: any[], category: string) => {
   return quiz.map(q => ({ ...q, Category: category }));
 };
 
-export const QUESTION_NUMS = [10, 25, 50, 100];
+export const QUESTION_NUMS = [5, 10, 25, 50, 100];
 
 export const CATEGORIES = [
-  "HTML",
-  "CSS",
-  "JavaScript",
   "Accessibility",
+  "Agile",
+  "Cloud Computing",
+  "CSS",
+  "DevOps",
   "General CS",
+  "Git",
+  "HTML",
   "IT",
+  "JavaScript",
   "Linux",
   "Python",
+  "QA",
+  "Regex",
+  "Security",
   "SQL"
-];
+].sort();
 
 export const ALL_CATEGORIES = [
   ...mapQuizCategory(accessibilityQuiz, "Accessibility"),
-  ...mapQuizCategory(cssQuiz, "CSS"),
+  ...mapQuizCategory(agileQuiz, "Agile"),
   ...mapQuizCategory(cloudComputingQuiz, "Cloud Computing"),
+  ...mapQuizCategory(cssQuiz, "CSS"),
   ...mapQuizCategory(devopsQuiz, "DevOps"),
-  ...mapQuizCategory(freecodecampQuiz, "freeCodeCamp"),
   ...mapQuizCategory(generalCSQuiz, "General CS"),
   ...mapQuizCategory(gitQuiz, "Git"),
   ...mapQuizCategory(htmlQuiz, "HTML"),
@@ -47,11 +54,10 @@ export const ALL_CATEGORIES = [
   ...mapQuizCategory(javascriptQuiz, "JavaScript"),
   ...mapQuizCategory(linuxQuiz, "Linux"),
   ...mapQuizCategory(pythonQuiz, "Python"),
-  ...mapQuizCategory(sqlQuiz, "SQL"),
-  ...mapQuizCategory(agileQuiz, "Agile"),
   ...mapQuizCategory(qualityAssuranceQuiz, "QA"),
+  ...mapQuizCategory(regexQuiz, "Regex"),
   ...mapQuizCategory(securityQuiz, "Security"),
-  ...mapQuizCategory(regexQuiz, "Regex")
+  ...mapQuizCategory(sqlQuiz, "SQL")
 ];
 
 export const ROUNDED_QUESTION_COUNT = 1200;
