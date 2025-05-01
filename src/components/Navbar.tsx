3b6e493e55bb90152a7fc2e111c66620b86db548
Ross Campbell <ross.campbell@mosaicx.com>
2025-05-01 18:23:09 -0500

    rename logo component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import '../stylesheets/Navbar.css';
 import React from 'react';
-// import fccLogo from "../images/fcc_primary_large.webp";
+import dqLogo from '../images/dq-favicon.png';
 
 const Navbar: React.FC = () => {
   return (
     <header>
       <nav className="homepage-navbar">
         <a target="_blank" rel="noreferrer" href="https://www.rosscampbell.dev/">
-          {/*<img className="website-logo" src={fccLogo} alt="Ross Campbell logo" />*/}
+          <img className="website-logo" src={dqLogo} alt="Ross Campbell logo" />
         </a>
       </nav>
     </header>
