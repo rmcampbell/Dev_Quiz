fd98bb0e070af688fa3a263a01b00b745595b9f3
Ross Campbell <ross.campbell@mosaicx.com>
2025-05-06 09:25:41 -0500

    tests are probably all broken, but committing changes anyways

--- src/__tests__/Data.test.tsx
+++ src/__tests__/Data.test.tsx
@@ -5,11 +5,9 @@ describe('Every question is valid', () => {
   test.each(ALL_CATEGORIES)(
     'Every question has an answer, explanation, a link and three distractions',
     input => {
-      expect(input).toHaveProperty('Question');
-      expect(input).toHaveProperty('Answer');
-      expect(input).toHaveProperty('Distractor1');
-      expect(input).toHaveProperty('Distractor2');
-      expect(input).toHaveProperty('Distractor3');
+      expect(input).toHaveProperty('question');
+      expect(input).toHaveProperty('answer');
+      expect(input).toHaveProperty('Distractors');
       expect(input).toHaveProperty('Explanation');
       expect(input).toHaveProperty('Link');
     }
@@ -17,13 +15,11 @@ describe('Every question is valid', () => {
   test.each(ALL_CATEGORIES)(
     'There are no empty answers, explanations,links or distractions.',
     input => {
-      expect(input.Question.length).toBeGreaterThan(0);
-      expect(input.Answer.length).toBeGreaterThan(0);
-      expect(input.Distractor1.length).toBeGreaterThan(0);
-      expect(input.Distractor2.length).toBeGreaterThan(0);
-      expect(input.Distractor3.length).toBeGreaterThan(0);
-      expect(input.Explanation.length).toBeGreaterThan(0);
-      expect(input.Link.length).toBeGreaterThan(0);
+      expect(input.question.length).toBeGreaterThan(0);
+      expect(input.answer.length).toBeGreaterThan(0);
+      expect(input.distractors.length).toBeGreaterThan(0);
+      expect(input.explanation.length).toBeGreaterThan(0);
+      expect(input.link.length).toBeGreaterThan(0);
     }
   );
 });
