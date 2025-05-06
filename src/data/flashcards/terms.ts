4fc35266f01aedf7f2f723bf36eb8d65c5ca6811
Ross Campbell <ross.campbell@mosaicx.com>
2025-05-06 09:30:44 -0500

    start on the structure

---
+++ src/data/flashcards/terms.ts
@@ -0,0 +1,88 @@
+const terms = [
+  {
+    term: '',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: '',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: '',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: '',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: '',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: '',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Collision Resistance',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Key Stretching',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Port Mirroring',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Port Security',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Rainbow Table',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Salting',
+    definition: '',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Separation of duties',
+    definition: 'Helps prevent fraud by ensuring no single person has control over all aspects of a process.',
+    notes: '',
+    example: ''
+  },
+  {
+    term: 'Split Knowledge',
+    definition: '',
+    notes: '',
+    example: ''
+  }
+];
+
+export default terms;
