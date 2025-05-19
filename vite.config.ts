103975600eb58a9308e5e394562b8c50d07ba1ab
Ross Campbell <rcampbell096@gmail.com>
2025-05-19 16:27:33 +0000

    fix deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'dev_quiz/',
   server: {
     watch: {
       // required for container hot reloading
