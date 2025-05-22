125cbb8c4162a168e3abe62b1b3b2aff1a1fcc37
Ross Campbell <ross.campbell@mosaicx.com>
2025-05-22 07:45:11 -0500

    enable test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-// import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-
+// import { defineConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   base: 'dev_quiz/',
+  define: {
+    'process.env': {}
+  },
   server: {
     watch: {
       // required for container hot reloading
@@ -18,16 +20,30 @@ export default defineConfig({
   build: {
     outDir: './dist',
     emptyOutDir: true
+  },
+  test: {
+    ...configDefaults,
+    globals: true,
+    ui: true,
+    uiBase: '/__vitest__/', // optional but good to have
+    environment: 'happy-dom',
+    setupFiles: './testSetup.ts',
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: './coverage',
+      reporter: ['text', 'html'],
+      enabled: true,
+      all: true
+    },
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/*'
+    ],
+    watch: false, // optional
+    api: {
+      port: 3100, // or another port you know is free
+      host: '127.0.0.1' // instead of 0.0.0.0
+    }
   }
-  // test: {
-  //   ...configDefaults,
-  //   globals: true,
-  //   environment: "happy-dom",
-  //   setupFiles: "./testSetup.ts",
-  //   exclude: [
-  //     "**/node_modules/**",
-  //     "**/dist/**",
-  //     "**/e2e/*" /* do not include playwright files */
-  //   ]
-  // }
 });
